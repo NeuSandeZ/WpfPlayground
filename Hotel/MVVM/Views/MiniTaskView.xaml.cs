015659adda93362d5c6c840099f979c5b5f123e7
Wojciech Massalski <smileyu@interia.pl>
2023-12-19 23:56:58 +0100

    added small views for Tasks

---
+++ Hotel/MVVM/Views/MiniTaskView.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace Hotel.MVVM.Views;
+
+public partial class MiniTaskView : UserControl
+{
+    public MiniTaskView()
+    {
+        InitializeComponent();
+    }
+}
